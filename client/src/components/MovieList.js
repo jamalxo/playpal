f61137745c667b21266858e8aedca7cbdd32e12a
Alex Feldsinger <alex.felds@freenet.de>
2020-06-11 14:09:14 +0200

    removed includes

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -6,12 +6,6 @@ import { DataTable, TableHeader, TableBody, TableRow, TableColumn, Button } from
 import { MovieListRow } from './MovieListRow';
 import Page from './Page'
 
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import EditIcon from '@material-ui/icons/Edit';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import NavigationIcon from '@material-ui/icons/Navigation';
-
 const dataTableStyle = {
   'marginBottom': '36px'
 };
