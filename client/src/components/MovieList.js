74ce467d16e0379d1394727c21c159db2d9d7cd2
Alex Feldsinger <alex.felds@freenet.de>
2020-06-11 13:47:33 +0200

    added buttons

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -6,6 +6,12 @@ import { DataTable, TableHeader, TableBody, TableRow, TableColumn, Button } from
 import { MovieListRow } from './MovieListRow';
 import Page from './Page'
 
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import NavigationIcon from '@material-ui/icons/Navigation';
+
 const dataTableStyle = {
   'marginBottom': '36px'
 };
@@ -25,6 +31,19 @@ export const MovieList = ({data, onDelete}) => (
                 {data.map((movie, i) => <MovieListRow key={i} movie={movie} onDelete={(id) => onDelete(id)} />)}
             </TableBody>
         </DataTable>
+        <Fab color="primary" aria-label="add">
+            <AddIcon />
+        </Fab>
+        <Fab color="secondary" aria-label="edit">
+            <EditIcon />
+        </Fab>
+        <Fab variant="extended">
+            <NavigationIcon />
+            Navigate
+        </Fab>
+        <Fab disabled aria-label="like">
+            <FavoriteIcon />
+        </Fab>
     </Page>
 );
 
