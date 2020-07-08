b4511d3990aca74188940157ed8511e5a9fce18a
edincitaku <edin.citaku@gmail.com>
2020-07-08 15:40:45 +0200

    added Offerlist and Offercard

---
+++ client/src/components/Offer/OfferList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import {Link} from "react-router-dom";
+import OfferCard from "./OfferCard";
+
+export default function OfferList(props) {
+    return <Grid container justify="center">
+
+        {props.dataOffers.map((item, index) => (
+            <Grid key={index} item className="profileList">
+
+                <Link key={index} className="linkDecoration" to={`/user/${item._id}`}>
+                    <OfferCard key={index} price={item.price} game={item.game} profile={item.owner}/>
+                </Link>
+            </Grid>
+        ))}
+    </Grid>
+}
