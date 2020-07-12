6baf8e019f30c3e71d50faf421ad1925eb0defeb
Jamal Sohail <s-sohail@hotmail.de>
2020-07-12 23:34:16 +0200

    edit only works in my offers

--- client/src/components/PendingRequest/PendingRequest.js
+++ client/src/components/PendingRequest/PendingRequest.js
@@ -1,20 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import ProfileService from "../../services/ProfileService";
 import CheckIcon from '@material-ui/icons/Check';
 import IconButton from "@material-ui/core/IconButton";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import ListItemText from "@material-ui/core/ListItemText";
 import ClearIcon from '@material-ui/icons/Clear';
-import {ThemeProvider as MuiThemeProvider} from "@material-ui/styles";
+import {ThemeProvider as MuiThemeProvider} from "@material-ui/core/styles";
 import {theme} from "../../theme";
 import {makeStyles} from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
-import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
-import {Link} from "react-router-dom";
 import CardMedia from "@material-ui/core/CardMedia";
 import {getGameIcon} from "../../services/IconService";
 import InfoIcon from '@material-ui/icons/Info';
@@ -58,7 +53,7 @@ export function PendingRequest(props) {
 
             var newprof
             const newoffer = await OfferService.getOffer(request.offer)
-            if(UserService.getCurrentUser().playertype ==="casual"){
+            if (UserService.getCurrentUser().playertype === "casual") {
                 newprof = await ProfileService.getProfile(offer.owner)
             }
             newprof = await ProfileService.getProfile(request.requestingPlayer)
@@ -70,84 +65,87 @@ export function PendingRequest(props) {
     }, []);
 
     return (
-        <Box borderRadius="50%">
-            <ListItem classes={{root: classes.root}} key={props.key} button>
-                <Grid
-                    container
-                    direction="row"
-                    justify="space-between"
-                    alignItems="flex-end"
-                >
-                    <Grid item>
-                        <Grid container
-                              direction="row"
-                              alignItems="center"
-                              justify="center"
-                              spacing={1}
+        <MuiThemeProvider theme={theme}>
+            <Box borderRadius="50%">
+                <ListItem classes={{root: classes.root}} key={props.key} button>
+                    <Grid
+                        container
+                        direction="row"
+                        justify="space-between"
+                        alignItems="flex-end"
+                    >
+                        <Grid item>
+                            <Grid container
+                                  direction="row"
+                                  alignItems="center"
+                                  justify="center"
+                                  spacing={1}
 
-                        >
-                            <Grid item>
-                                <Box ml={1}>
-                                    <Avatar
-                                        className="profilePicture"
-                                        alt={requestProfile.username}
-                                        title={requestProfile.username}
-                                        src={requestProfile.profileImage}
-                                        className={classes.small}
+                            >
+                                <Grid item>
+                                    <Box ml={1}>
+                                        <Avatar
+                                            className="profilePicture"
+                                            alt={requestProfile.username}
+                                            title={requestProfile.username}
+                                            src={requestProfile.profileImage}
+                                            className={classes.small}
 
-                                    />
-                                </Box>
-                            </Grid>
-                            <Grid item>
-                                {requestProfile.username}
+                                        />
+                                    </Box>
+                                </Grid>
+                                <Grid item>
+                                    {requestProfile.username}
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
 
-                    <Grid item>
-                        <Grid container
-                              direction="row"
-                              alignItems="center"
-                              justify="center"
-                        >
-                            <CardMedia src={getGameIcon(request.game)} component="img" className={classes.imageStyle}/>
+                        <Grid item>
+                            <Grid container
+                                  direction="row"
+                                  alignItems="center"
+                                  justify="center"
+                            >
+                                <CardMedia src={getGameIcon(request.game)} component="img"
+                                           className={classes.imageStyle}/>
 
-                            {request.game}
+                                {request.game}
+                            </Grid>
+                        </Grid>
+                        <Grid item>
+                            <IconButton edge="end" aria-label="info" onClick={() => setDialogOpen(true)}>
+                                <InfoIcon/>
+                            </IconButton>
+
+                            <IconButton edge="end" aria-label="delete">
+                                <CheckIcon/>
+                            </IconButton>
+                            <IconButton edge="end" aria-label="delete">
+                                <ClearIcon/>
+                            </IconButton>
+
                         </Grid>
                     </Grid>
-                    <Grid item>
-                        <IconButton edge="end" aria-label="info" onClick={()=> setDialogOpen(true)}>
-                            <InfoIcon/>
-                        </IconButton>
+                    <Dialog
+                        open={dialogOpen}
+                        onClose={() => setDialogOpen(false)}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                    >
+                        <DialogTitle id="alert-dialog-title">{"Additional Request information"}</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                                {request.info}                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={() => setDialogOpen(false)} color="primary" autoFocus>
+                                OK
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
 
-                        <IconButton edge="end" aria-label="delete">
-                            <CheckIcon/>
-                        </IconButton>
-                        <IconButton edge="end" aria-label="delete">
-                            <ClearIcon/>
-                        </IconButton>
-
-                    </Grid>
-                </Grid>
-                <Dialog
-                    open={dialogOpen}
-                    onClose={() => setDialogOpen(false)}
-                    aria-labelledby="alert-dialog-title"
-                    aria-describedby="alert-dialog-description"
-                >
-                    <DialogTitle id="alert-dialog-title">{"Additional Request information"}</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText id="alert-dialog-description">
-                            {request.info}                            </DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={() => setDialogOpen(false)} color="primary" autoFocus>
-                            OK
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-
-            </ListItem>
-        </Box>
+                </ListItem>
+            </Box>
+        </MuiThemeProvider>
     )
 }
--- client/src/views/PendingRequestsView.js
+++ client/src/views/PendingRequestsView.js
@@ -2,14 +2,10 @@ import React from 'react';
 import UserService from "../services/UserService";
 import Page from "../components/Page/Page";
 import {PendingRequest} from "../components/PendingRequest/PendingRequest";
-import List from "@material-ui/core/List";
 import Grid from "@material-ui/core/Grid";
-import OfferCard from "../components/OfferCard/OfferCard";
 import {ThemeProvider as MuiThemeProvider} from "@material-ui/styles";
 import {theme} from "../theme";
-import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import ListItem from "@material-ui/core/ListItem";
 import Box from "@material-ui/core/Box";
 
 const useStyles = (theme) => ({
@@ -20,7 +16,7 @@ const useStyles = (theme) => ({
         fontWeight: '450',
         color: theme.palette.primary.contrastText,
         alignSelf: 'center',
-        marginLeft:'25px',
+        marginLeft: '25px',
     },
     headerAccept: {
         fontWeight: '450',
@@ -30,72 +26,72 @@ const useStyles = (theme) => ({
 
 
 })
+
 export function PendingRequestsView() {
 
     const requests = UserService.getCurrentUser().pendingRequests
     const test = [
-        {requestingPlayer: "5edb984f34c7ee3ba785d0a5", game:"LoL", info: "I would like to play this tuesday"},
-        {requestingPlayer: "5f01c25ed6e0f768a582d22b", game: "CS:GO",  info: "I would like to play this tuesday"},
-        {requestingPlayer: "5f00ce545696f14503afdc63", game: "DotA 2", info: "I would like to play this tuesday"},
-        {requestingPlayer: "5edb984f34c7ee3ba785d0a5", game:"LoL", info: "I would like to play this tuesday"},
+        {requestingPlayer: "5edb984f34c7ee3ba785d0a5", game: "LoL", info: "I would like to play this tuesday"},
         {requestingPlayer: "5f01c25ed6e0f768a582d22b", game: "CS:GO", info: "I would like to play this tuesday"},
         {requestingPlayer: "5f00ce545696f14503afdc63", game: "DotA 2", info: "I would like to play this tuesday"},
-        {requestingPlayer: "5edb984f34c7ee3ba785d0a5", game:"LoL", info: "I would like to play this tuesday"},
+        {requestingPlayer: "5edb984f34c7ee3ba785d0a5", game: "LoL", info: "I would like to play this tuesday"},
+        {requestingPlayer: "5f01c25ed6e0f768a582d22b", game: "CS:GO", info: "I would like to play this tuesday"},
+        {requestingPlayer: "5f00ce545696f14503afdc63", game: "DotA 2", info: "I would like to play this tuesday"},
+        {requestingPlayer: "5edb984f34c7ee3ba785d0a5", game: "LoL", info: "I would like to play this tuesday"},
         {requestingPlayer: "5f01c25ed6e0f768a582d22b", game: "CS:GO", info: "I would like to play this tuesday"},
         {requestingPlayer: "5f00ce545696f14503afdc63", game: "DotA 2", info: "I would like to play this tuesday"},
 
     ]
     const classes = useStyles(theme)
-    return(
+    return (
         <Page>
-
             <MuiThemeProvider theme={theme}>
-<div style={{marginTop:'150px'}}>
-            <Grid
-                container
-                direction="column"
-                  justify="center"
-                  alignItems="center"
-            >
-                <Typography variant="h4" className={classes.headerPlayPal} align="center">
-                    PENDING REQUESTS
-                </Typography>
-                <div style={{width: '1000px'}}>
-                <Grid
-                    container
-                    direction="row"
-                    justify="space-between"
-                    alignItems="flex-end"
-                >
-                    <Grid item>
-                        <Box ml={1}>
-                    <Typography variant="h5" className={classes.headerPlayPal} align="center">
-                        Player
-                    </Typography>
-                        </Box>
-                    </Grid>
-                    <Grid item>
-                        <Box ml={8}>
+                <div style={{marginTop: '150px'}}>
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <Typography variant="h4" className={classes.headerPlayPal} align="center" color="textPrimary">
+                            PENDING REQUESTS
+                        </Typography>
+                        <div style={{width: '1000px'}}>
+                            <Grid
+                                container
+                                direction="row"
+                                justify="space-between"
+                                alignItems="flex-end"
+                            >
+                                <Grid item>
+                                    <Box ml={1}>
+                                        <Typography variant="h5" className={classes.headerPlayPal} align="center" color="textPrimary">
+                                            Player
+                                        </Typography>
+                                    </Box>
+                                </Grid>
+                                <Grid item>
+                                    <Box ml={8}>
 
-                        <Typography variant="h5" className={classes.headerPlayPal} align="center">
-                            Game
-                        </Typography>
-                        </Box>
+                                        <Typography variant="h5" className={classes.headerPlayPal} align="center" color="textPrimary">
+                                            Game
+                                        </Typography>
+                                    </Box>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant="h5" className={classes.headerAccept} align="center" color="textPrimary">
+                                        Accept/Decline
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </div>
+                        <Grid item className="PendingOfferList">
+                            {
+                                test.map((item, index) => <PendingRequest request={item} index={index}/>)
+                            }
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <Typography variant="h5" className={classes.headerAccept} align="center">
-                            Accept/Decline
-                        </Typography>
-                    </Grid>
-                </Grid>
                 </div>
-                <Grid  item className="PendingOfferList">
-            {
-                test.map((item, index) => <PendingRequest request={item} index={index}/>)
-            }
-                    </Grid>
-            </Grid>
-</div>
             </MuiThemeProvider>
         </Page>
     )
